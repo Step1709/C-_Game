bf3be22da05c47f7a93d4c0835b7764eadeb255f
ivolzok <ivolzok@gmail.com>
2025-05-30 22:31:21 +0500

    доработка обучения и интерфейса

--- Assets/Scripts/Training/SwitchInstruction.cs
+++ Assets/Scripts/Training/SwitchInstruction.cs
@@ -10,9 +10,11 @@ namespace Training
         
         [SerializeField] private GameObject instructions;
         [SerializeField] private TextMeshProUGUI instructionsText;
+        [SerializeField] private ChangeFree changeFree;
 
         void OnEnable()
         {
+            changeFree.enabled = false;
             instructions.SetActive(true);
             instructionsText.text = Instructions.Dequeue();
         }
@@ -22,6 +24,7 @@ namespace Training
             {
                 if (Instructions.Count == 0)
                 {
+                    changeFree.enabled = true;
                     enabled = false;
                     instructions.SetActive(false);
                     instructionsText.text = "";
